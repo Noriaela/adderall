6dcb45db9d871b56280b0bb5f4741d9ea9d43539
sonicrules11 <sonic886310@gmail.com>
2020-02-14 17:33:09 -0700

    Sorted stuff with comments so it's easier to read for me

--- dependencies/common_includes.hpp
+++ dependencies/common_includes.hpp
@@ -18,14 +18,22 @@
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
 
+// Utilities
 #include "utilities/fnv.hpp"
 #include "utilities/utilities.hpp"
 #include "utilities/render.hpp"
-#include "utilities/hook.hpp"
-#include "interfaces/interfaces.hpp"
-#include "utilities/netvars.hpp"
+#include "utilities/netvar_manager.hpp"
 
+// Interfaces
+#include "interfaces/interfaces.hpp"
+
+// SDK
 #include "../source-sdk/sdk.hpp"
+
+// Hooks
+#include "utilities/hook.hpp"
 #include "../core/hooks/hooks.hpp"
 
+// Features
 #include "../core/features/config.h"
+#include "../core/features/misc.h"
