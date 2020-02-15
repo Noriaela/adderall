61846b9a01487a5b4f981f5eb5d1121227430698
sonicrules11 <sonic886310@gmail.com>
2020-02-14 17:40:07 -0700

    Added bhop

---
+++ core/features/misc.cpp
@@ -0,0 +1,39 @@
+#include "misc.h"
+
+#include "../../dependencies/common_includes.hpp"
+
+c_misc misc;
+
+static bool should_jump = false;
+static bool should_fake = false;
+
+void c_misc::do_bhop(c_usercmd* cmd) {
+	if (!set.do_bhop)
+		return;
+
+	auto local_player = reinterpret_cast<player_t*>(interfaces::entity_list->get_client_entity(interfaces::engine->get_local_player()));
+
+	if (!local_player)
+		return;
+	
+	// Add check for different movetypes
+
+	if (should_fake && !should_jump) {
+		cmd->buttons |= in_jump;
+		should_fake = false;
+	}
+	else if (cmd->buttons & in_jump) {
+		if (local_player->flags() & fl_onground) {
+			should_fake = false;
+			should_jump = false;
+		}
+		else {
+			cmd->buttons &= ~in_jump;
+			should_jump = false;
+		}
+	}
+	else {
+		should_jump = false;
+		should_fake = false;
+	}
+}
---
+++ core/features/misc.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../../dependencies/common_includes.hpp"
+
+class c_misc {
+
+public:
+
+	void do_bhop(c_usercmd* cmd);
+};
+
+extern c_misc misc;
