8d9660891438530e2b1b5716f6b047bfb1134d96
sonicrules11 <sonic886310@gmail.com>
2020-02-14 19:01:19 -0700

    added antiaim

---
+++ core/features/antiaim/antiaim.cpp
@@ -0,0 +1,7 @@
+#include "antiaim.h"
+
+#include "../../../dependencies/common_includes.hpp"
+
+c_antiaim antiaim;
+
+// For antiaim
---
+++ core/features/antiaim/antiaim.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../../../dependencies/common_includes.hpp"
+
+class c_antiaim {
+
+public:
+
+private:
+
+};
+
+extern c_antiaim antiaim;
