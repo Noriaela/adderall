e541799bb220d232e9ac13c56d58480cd5769333
sonicrules11 <sonic886310@gmail.com>
2020-02-14 17:32:43 -0700

    changed some names and changed checkbox test to bhop

--- core/menu/menu.cpp
+++ core/menu/menu.cpp
@@ -3,19 +3,16 @@
 c_menu g_menu;
 
 void c_menu::render( ) {
-	static bool checkbox;
-	static float sliderf;
-	static int slideri, dropdown;
 
 	zgui::poll_input( "Counter-Strike: Global Offensive" );
 
-	if ( zgui::begin_window( "sdk for counter-strike: global offensive", { 500, 350 }, render::main_font ) ) {
-		zgui::begin_groupbox( "Example groupbox", { 468, 311 } );
+	if ( zgui::begin_window( "adderall", { 500, 350 }, render::main_font ) ) {
+		zgui::begin_groupbox( "main group", { 468, 311 } );
 		{
-			zgui::checkbox( "Example checkbox", checkbox );
-			zgui::slider_float( "Example slider (float)", 0.0f, 100.0f, sliderf );
-			zgui::slider_int( "Example slider (int)", 0, 100, slideri );
-			zgui::combobox( "Example dropdown", { "aaaa", "bbbb", "cccc", "dddd" }, dropdown );
+			zgui::checkbox( "bunny hop", set.do_bhop );
+			//zgui::slider_float( "Example slider (float)", 0.0f, 100.0f, sliderf );
+			//zgui::slider_int( "Example slider (int)", 0, 100, slideri );
+			//zgui::combobox( "Example dropdown", { "aaaa", "bbbb", "cccc", "dddd" }, dropdown );
 		}
 		zgui::end_groupbox( );
 
